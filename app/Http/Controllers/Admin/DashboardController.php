8f8509c9edd66584bfb254ebd2f30a1eb2c4c1a2
Jidai <lopesdacruzjeray@gmail.com>
2026-02-15 15:00:39 +0800

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
